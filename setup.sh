48d232d2f7f792361ee55701d5eb3da4337a0e27
Tibor Bödecs <mail.tib@gmail.com>
2020-01-14 13:58:11 +0800

    setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+if which llvm-cov >/dev/null; then
+    echo "[INFO] llvm-cov already available"
+    exit
+fi
+
+echo "[INFO] installing llvm-cov..."
+
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+brew install llvm
+
+if [ -n \"$ZSH_VERSION\" ]; then
+    echo 'export PATH=\"/usr/local/opt/llvm/bin:$PATH\"' >> ~/.zshrc
+    source ~/.zshrc
+else
+    echo 'export PATH=\"/usr/local/opt/llvm/bin:$PATH\"' >> ~/.bash_profile
+    source ~/.bash_profile
+fi
+
