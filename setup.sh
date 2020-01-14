01e32d759496b8b9d66eead8066d28e81902f646
Tibor Bödecs <mail.tib@gmail.com>
2020-01-14 14:05:25 +0800

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ export HOMEBREW_NO_INSTALL_CLEANUP=1
 
 brew install llvm
 
-if [ -n \"$ZSH_VERSION\" ]; then
-    echo 'export PATH=\"/usr/local/opt/llvm/bin:$PATH\"' >> ~/.zshrc
+if [ -n "$ZSH_VERSION" ]; then
+    echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.zshrc
     source ~/.zshrc
 else
-    echo 'export PATH=\"/usr/local/opt/llvm/bin:$PATH\"' >> ~/.bash_profile
+    echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.bash_profile
     source ~/.bash_profile
 fi
 
